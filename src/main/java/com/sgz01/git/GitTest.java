17c3780b2062475d07506e3df5f34535f996f0b3
sgz02 <ww@qq.com>
2022-04-22 23:06:54 +0800

    commit01

---
+++ src/main/java/com/sgz01/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.sgz01.git;
+
+/**
+ * @Date:2022/4/22 22:55
+ * @Author:Shen-Childe
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git IDEA");
+    }
+}
