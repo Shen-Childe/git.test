b1517d09d152c687ccc3567793cdfca65ec74a2d
sgz02 <ww@qq.com>
2022-04-24 19:16:02 +0800

    commit02

--- src/main/java/com/sgz01/git/GitTest.java
+++ src/main/java/com/sgz01/git/GitTest.java
@@ -7,5 +7,7 @@ package com.sgz01.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git IDEA");
+        System.out.println("hello git IDEA");
+
     }
 }
