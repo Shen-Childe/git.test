f78079f164beff18f8b7fc24e08904cbcb7196ca
Shen-Childe <87227653+Shen-Childe@users.noreply.github.com>
2022-04-24 19:50:49 +0800

    Update GitTest.java

--- src/main/java/com/sgz01/git/GitTest.java
+++ src/main/java/com/sgz01/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("hello git IDEA5");
         System.out.println("hello git IDEA5-master");
         System.out.println("hello git IDEA5");
+        System.out.println("修改的");
     }
 }
